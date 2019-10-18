45082dd837be0615a1f8a668ecde49a60c881ac8
Darrin Lowery <lambda@monsto.com>
2019-10-18 11:38:57 -0500

    wasted time doing the  thing wrong

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,14 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
 
-function clg(...x) { for (let exes of x) console.log(exes); }
+function clg(...x) {
+	for (let exes of x) console.log(exes);
+}
 
-const api = "https://rickandmortyapi.com/api/character/"
+const api = "https://rickandmortyapi.com/api/character/";
 
 export default function CharacterList() {
 	// TODO: Add useState to track data from useEffect
 	const [allchars, setChars] = useState([]);
-
 
 	useEffect(() => {
 		// TODO: Add API Request here - must run in `useEffect`
@@ -21,7 +22,6 @@ export default function CharacterList() {
 			.get(api)
 			.then(response => {
 				setChars(response.data.results);
-				clg(response.data.results);
 			})
 			.catch(error => {
 				console.error(error);
@@ -33,14 +33,11 @@ export default function CharacterList() {
 	return (
 		<section className="character-list">
 			{/* <h2>TODO: `array.map()` over your state here!</h2> */}
-			{
-				allchars.map(e => {
-					// clg("------",e.name)
-					// return (<div>{e.name}</div>)
-					return (<CharacterCard key={e.id} name={e.name} status={e.status} />)
-
-				})
-			}
+			{allchars.map(e => {
+				// clg("------",e.name)
+				// return (<div>{e.name}</div>)
+				return <CharacterCard key={e.id} name={e.name} status={e.status} />;
+			})}
 		</section>
 	);
 }
